299ca61f9ecdfae77f5844303023503369cb2bc4
Lucy Arquejada <arquejadalucy@gmail.com>
2023-05-24 12:29:37 -0300

    [1] format

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,14 @@
 from app.enums import TRIBUNAIS_VALIDOS, Tribunais
 
-process_request_body_schema = {
-    'numero_processo': {'type': 'string', 'required': True}
-}
-
 
 def tribunal_suportado(field, value, error):
     if value not in TRIBUNAIS_VALIDOS:
         error(field, f"Tribunal não suportado. Tribunais válidos: {[tribunal.name for tribunal in Tribunais]}")
 
+
+process_request_body_schema = {
+    'numero_processo': {'type': 'string', 'required': True}
+}
 
 id_processo_schema = {'id': {'type': 'string', 'nullable': False,
                              'regex': "\d\d\d\d\d\d\d-\d\d\.\d\d\d\d\.\d\.\d\d\.\d\d\d\d"}}
