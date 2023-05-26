509af0695d20cde64c9e870dff73d058a6ce8f2a
Lucy Arquejada <arquejadalucy@gmail.com>
2023-05-25 21:30:44 -0300

    Revert "[1] templates"
    This reverts commit 79dd3febaa06a4d28b7090639136872e00666cd6.

--- app/api.py
+++ app/api.py
@@ -1,7 +1,5 @@
-import asyncio
-
 from cerberus import Validator
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -12,33 +10,17 @@ from app.schemas import process_request_informations_schema, id_processo_schema
 
 app = FastAPI()
 validator = Validator()
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-templates = Jinja2Templates(directory="app/templates")
 
 
+#
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+# templates = Jinja2Templates(directory="templates")
 def valid_request(processo_info: ProcessRequestInformations):
     return validator.validate(processo_info.__dict__, process_request_informations_schema)
 
 
 def valid_process_id(numero_processo: str):
     return validator.validate({"id": numero_processo}, id_processo_schema)
-
-
-@app.get('/', response_class=HTMLResponse, include_in_schema=False)
-def main(request: Request):
-    return templates.TemplateResponse('home.html', {'request': request})
-
-
-@app.post('/dadosprocesso', include_in_schema=False)
-def busca_processo_por_form(id_processo: str = Form(...)):
-    if not valid_process_id(id_processo):
-        return validator.errors
-
-    processo_info = ProcessRequestInformations(id_processo)
-
-    if not valid_request(processo_info):
-        return validator.errors
-    return search_process_data(processo_info)
 
 
 @app.post("/processo")
@@ -55,7 +37,7 @@ async def buscar_processo(process_request: ProcessRequestBody):
     return response
 
 
-@app.get("/processos/{id_processo}")
+@app.get("/processo/{id_processo}")
 def get_processo_info_by_id(id_processo: str):
     if not valid_process_id(id_processo):
         return validator.errors
