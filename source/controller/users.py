5fb38a10368b381eaca44c9b2e0a734d02878178
arquejadalucy <lucy.velasco@jusbrasil.com.br>
2023-12-05 22:16:28 -0300

    start front login e cadastro

--- source/controller/users.py
+++ source/controller/users.py
@@ -1,10 +1,13 @@
 import pyrebase
 import firebase_admin
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from fastapi.requests import Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.exceptions import HTTPException
 from firebase_admin import credentials, auth
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from source.config.settings import FIREBASE_CONFIG
 from source.schemas.AuthenticationSchemas import SignUpSchema, LoginSchema
 
@@ -27,13 +30,13 @@ async def create_an_account(user_data: SignUpSchema):
 
     try:
         user = auth.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
         return JSONResponse(content={"message": f"User account created successfuly for user {user.uid}"},
                             status_code=201
-        )
+                            )
     except auth.EmailAlreadyExistsError:
         raise HTTPException(
             status_code=400,
@@ -51,15 +54,13 @@ async def create_access_token(user_data: LoginSchema):
             email=email,
             password=password
         )
-
         token = user['idToken']
-
         return JSONResponse(
             content={
                 "token": token
             }, status_code=200
         )
-    except:
+    except HTTPException:
         raise HTTPException(
             status_code=400,
             detail="Invalid credentials"
@@ -74,3 +75,20 @@ async def validate_token(request: Request):
     user = auth.verify_id_token(jwt)
 
     return user['user_id']
+
+
+def get_jinja_templates():
+    router.mount("/static", StaticFiles(directory="front-end/static"), name="static")
+    return Jinja2Templates(directory="front-end/templates")
+
+
+@router.get('/login', response_class=HTMLResponse, tags=["home"], include_in_schema=False)
+def main(request: Request):
+    return get_jinja_templates().TemplateResponse('home.html', {'request': request})
+
+
+@router.post('/signin', include_in_schema=False)
+def buscar_processo_pelo_form(request: Request, email: str = Form(), password: str = Form()):
+    user = LoginSchema(email=email, password=password)
+    result = create_access_token(user)
+    return get_jinja_templates().TemplateResponse('home.html', {'request': request, 'result': result})
